787b0c2a4fdc5a600c0fdbcb5cecba76992bffd7
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-13 11:40:32 +1100

    Add `stdin` Test Helper for future testing
    Add a `stdin` override class to demonstrate replacing standard User
    input with a String instead. This will be used later on to test accept
    User input via test instead of prompting a User at a console.
    
    == References:
    - [refchan manual page \- Tcl Built\-In Commands]
      (https://www.tcl.tk/man/tcl8.6/TclCmd/refchan.htm#M21)
    
    - [channel \- Create Chan In TCL for specific app \- Stack Overflow]
      (https://stackoverflow.com/questions/25705873/create-chan-in-tcl-for-specific-app)

--- tests/TestHelpers.tcl
+++ tests/TestHelpers.tcl
@@ -36,3 +36,67 @@ oo::class create CapturingTransform {
         return ""
     }
 }
+
+# == Sourced:
+# - [refchan manual page \- Tcl Built\-In Commands]
+#   (https://www.tcl.tk/man/tcl8.6/TclCmd/refchan.htm#M21)
+#
+# == Usage Example:
+# Now we create an instance...
+#
+#     set string "The quick brown fox jumps over the lazy dog.\n"
+#     set ch [chan create read [stringchan new $string]]
+#
+#     puts [gets $ch];   # Prints the whole string
+#
+#     seek $ch -5 end;
+#     puts [read $ch];   # Prints just the last word
+#
+oo::class create stringchan {
+    variable data pos
+    constructor {string {encoding {}}} {
+        if {$encoding eq ""} {set encoding [encoding system]}
+        set data [encoding convertto $encoding $string]
+        set pos 0
+    }
+
+    method initialize {ch mode} {
+        return "initialize finalize watch read seek"
+    }
+    method finalize {ch} {
+        my destroy
+    }
+    method watch {ch events} {
+        # Must be present but we ignore it because we do not
+        # post any events
+    }
+
+    # Must be present on a readable channel
+    method read {ch count} {
+        set d [string range $data $pos [expr {$pos+$count-1}]]
+        incr pos [string length $d]
+        return $d
+    }
+
+    # This method is optional, but useful for the example above
+    method seek {ch offset base} {
+        switch $base {
+            start {
+                set pos $offset
+            }
+            current {
+                incr pos $offset
+            }
+            end {
+                set pos [string length $data]
+                incr pos $offset
+            }
+        }
+        if {$pos < 0} {
+            set pos 0
+        } elseif {$pos > [string length $data]} {
+            set pos [string length $data]
+        }
+        return $pos
+    }
+}
