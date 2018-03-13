9d5af448a97866f968a7e113bfa3d09d79aa4472
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-03-13 12:43:31 +1100

    Add Turn methods `left` & `right` to Robot
    Add Turn methods left and right to the ToyRobot Robot class that update
    the current instance of Robot facing direction, by turning from the
    current facing direction to one of four Compass directions; i.e. NORTH,
    EAST, SOUTH and WEST.
    
    == Notes:
    - For whatever reason TclOO does not have strong typing meaning the
      dictionary is actually a list/array that is being accessed like a
      `dict`, but also cannot easily create static variables without hiding
      them inside clojures, like private methods
    
    - Fix up the names used in some tests
    
    == References:
    - [Tcl \- dicts \- a tutorial and examples]
      (http://www.wellho.net/mouth/3614_Tcl-dicts-a-tutorial-and-examples.html)
    
    - [class manual page \-]
      (https://www.tcl.tk/man/tcl/ItclCmd/class.htm)
    
    - [TclOO Tricks]
      (https://nikit.tcl-lang.org/page/TclOO+Tricks)
    
    - [Static variable in Tcl \- Stack Overflow]
      (https://stackoverflow.com/questions/6122731/static-variable-in-tcl)

--- lib/ToyRobot.tcl
+++ lib/ToyRobot.tcl
@@ -1,11 +1,18 @@
 # package require TclOO;
 
 oo::class create Robot {
+    variable TURN
     variable x
     variable y
     variable facing
 
     constructor {{dx 0} {dy 0} {dfacing "NORTH"}} {
+        set TURN {
+          NORTH {LEFT WEST RIGHT EAST}
+          SOUTH {LEFT EAST RIGHT WEST}
+          EAST {LEFT NORTH RIGHT SOUTH}
+          WEST {LEFT SOUTH RIGHT NORTH}
+        }
         set x $dx
         set y $dy
         set facing $dfacing
@@ -17,5 +24,13 @@ oo::class create Robot {
 
     method report {} {
       puts "$x,$y,$facing"
+    }
+
+    method left {} {
+      set facing [dict get $TURN $facing LEFT]
+    }
+
+    method right {} {
+      set facing [dict get $TURN $facing RIGHT]
     }
 }
